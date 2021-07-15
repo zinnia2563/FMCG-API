f301d2088b2f7386f0b94b10156320912853cc02
zinni <zinnia854@gmail.com>
2021-07-15 08:06:21 +0600

    json response

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -4,13 +4,13 @@ const createOrderByImage = asyncHandler(async(req,res) => {
     console.log(req.body);
     try {
        res.json({
-           message: "we have got your request"
+           key: "Khusbo",
+           value: "rice"
        })
     } catch (error) {
         res.json({message: error});
     }   
   })
-
   module.exports = {
       createOrderByImage
   }
