c10a28172fc06c12166638829cb2104befd8f830
zinni <zinnia854@gmail.com>
2021-07-15 21:55:49 +0600

    product name updated

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -4,8 +4,8 @@ const createOrderByImage = asyncHandler(async(req,res) => {
     console.log(req.body);
     try {
         res.json({
-            key: "Khusbo",
-            value: "rice",
+
+            prduct_name: "rice",
             url: "https://digitalistic.co/app/product/1",
         })
        
