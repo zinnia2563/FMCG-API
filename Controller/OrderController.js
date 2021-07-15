48a87bacbdc22c19b179472dce65c1c377ee12d2
zinni <zinnia854@gmail.com>
2021-07-15 21:24:14 +0600

    url updated

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -3,10 +3,12 @@ const asyncHandler = require('express-async-handler');
 const createOrderByImage = asyncHandler(async(req,res) => {
     console.log(req.body);
     try {
-       res.json({
-           key: "Khusbo",
-           value: "rice"
-       })
+        res.json({
+            key: "Khusbo",
+            value: "rice",
+            url: "https://digitalistic.co/app/product/1",
+        })
+       
     } catch (error) {
         res.json({message: error});
     }   
